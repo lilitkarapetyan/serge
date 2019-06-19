86f24ebb5ac158d61bcb670ea78dab19b464887a
Ian Mayo <ian@planetmayo.com>
2019-06-19 18:27:25 +0100

    display time for feedback messages

--- client/src/Views/InsightsChannel.jsx
+++ client/src/Views/InsightsChannel.jsx
@@ -5,6 +5,7 @@ import {
   getAllWargameFeedback,
 } from "../ActionsAndReducers/playerUi/playerUi_ActionCreators";
 import '../scss/App.scss';
+import moment from "moment";
 
 class InsightsChannel extends Component {
 
@@ -39,6 +40,7 @@ class InsightsChannel extends Component {
                 <Badge pill variant="primary">{message.playerInfo.force}</Badge> 
                 <Badge pill variant="secondary">{message.playerInfo.role}</Badge>
                 {message.playerInfo.name && <Badge pill variant="warning">{message.playerInfo.name}</Badge>}
+                <span>{moment(message.timestamp).format("YYYY-MMM-DD HH:mm")}</span>
               </div>
               {message.message}
               <p className="feedback-marker"  style={{borderColor: message.playerInfo.forceColor}}></p>
