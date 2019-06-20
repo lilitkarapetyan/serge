fe468bcf1687ebeb5653e6698defc3d53b360fbb
Ian Mayo <ian@planetmayo.com>
2019-06-20 16:33:11 +0100

    use `secret` icon for private messages

--- client/src/Components/MessagePreviewPlayerUi.jsx
+++ client/src/Components/MessagePreviewPlayerUi.jsx
@@ -5,6 +5,13 @@ import '../scss/App.scss';
 import check from "check-types";
 import moment from "moment";
 import isValidUrl from "../Helpers/isValidUrl";
+
+import {
+  faUserSecret
+} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
 const Fragment = React.Fragment;
 
 class MessagePreview extends Component {
@@ -160,7 +167,8 @@ class MessagePreview extends Component {
         })}
         {this.props.privateMessage && (
           <>
-            <span className="detail">Private:</span>
+            <span className="detail"><FontAwesomeIcon icon={faUserSecret}/>
+Private:</span>
             <span className="data">{this.props.privateMessage.split('\n').map( (it, i) => <div key={'x'+i}>{it}</div> )}</span>
           </>
         )}
