df22fe186aeb03dda23ffe416a69e6489beed06c
Heri Setiawan <heri.dehero@gmail.com>
2019-06-28 09:15:59 +0700

    Fix channel collapsible panels bug: opening/closing state mixed up

--- client/src/Components/MessageListItem.jsx
+++ client/src/Components/MessageListItem.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-
 import '../scss/App.scss';
-
 import Badge from "react-bootstrap/Badge";
-
 import {
   faPlus,
   faMinus,
@@ -17,98 +14,101 @@ import MessagePreview from "../Components/MessagePreviewPlayerUi";
 class MessageListItem extends Component {
 
   constructor(props) {
+    const { currentWargame, detail } = props;
     super(props);
     this.state = {
-      hasBeenRead: !!window.localStorage.getItem(this.props.currentWargame + this.props.detail._id),
+      collapsed: true,
+      hasBeenRead: (() => {
+        return !!MessageListItem.getStorageState(currentWargame + detail._id);
+      })(),
     }
   }
 
-  componentWillUpdate(nextProps, nextState, nextContext) {
+  componentDidUpdate(nextProps, nextState, nextContext) {
+    const { currentWargame, detail, allMarkedRead } = this.props;
+    const markedAsReadStorageState = MessageListItem.getStorageState(currentWargame + nextProps.detail._id);
+    const hasBeenReadLocally  = markedAsReadStorageState === "read";
+    const hasNotBeenReadLocally = markedAsReadStorageState === null;
+    const selfReadAction = detail._id === nextProps.detail._id;
 
-    if (!this.props.allMarkedRead && nextProps.allMarkedRead) {
-      this.setState({
-        hasBeenRead: true,
-      });
-    }
+    const allMarkedAsRead = !allMarkedRead && nextProps.allMarkedRead;
+    const markedAsReadOnStorage = !this.state.hasBeenRead && selfReadAction && hasBeenReadLocally;
+    const markedAsRead    = !selfReadAction && hasBeenReadLocally;
+    const markedAsUnread  = !selfReadAction && hasNotBeenReadLocally;
+
     // react reuses the component with different data when the messages array updates.
     // perform check against component detail _id to see if it's a new message and set
     // hasBeenRead correctly.
-    if (
-      !this.state.hasBeenRead &&
-      this.props.detail._id === nextProps.detail._id &&
-      window.localStorage.getItem(this.props.currentWargame + nextProps.detail._id) === "read"
-    ) {
+    if ( allMarkedAsRead || markedAsReadOnStorage || markedAsRead ) {
       this.setState({
         hasBeenRead: true,
       })
-    }
-
-    if (
-      this.props.detail._id !== nextProps.detail._id &&
-      window.localStorage.getItem(this.props.currentWargame + nextProps.detail._id) === "read"
-    ) {
-      this.setState({
-        hasBeenRead: true,
-      })
-    }
-    if (
-      this.props.detail._id !== nextProps.detail._id &&
-      window.localStorage.getItem(this.props.currentWargame + nextProps.detail._id) === null
-    ) {
+    } else if ( markedAsUnread ) {
       this.setState({
         hasBeenRead: false,
       })
     }
   }
 
+  static getStorageState(key) {
+    return window.localStorage.getItem(key);
+  }
+
   open = () => {
-    this.props.open(this.props.detail);
-    window.localStorage.setItem(this.props.currentWargame + this.props.detail._id, "read");
+    const { detail, currentWargame } = this.props;
+    this.props.open(detail);
+    window.localStorage.setItem(currentWargame + detail._id, "read");
     this.setState({
+      collapsed: false,
       hasBeenRead: true,
     });
   };
 
   close = () => {
     this.props.close(this.props.detail);
+    this.setState({
+      collapsed: true,
+    });
   };
 
   render() {
-
+    const { detail, key } = this.props;
+    const { collapsed, hasBeenRead } = this.state;
+    const expanded = !collapsed || detail.isOpen;
     let itemTitle;
-    if (this.props.detail.message.title) {
-      itemTitle = this.props.detail.message.title;
-    } else if(this.props.detail.message.content) {
+    if (detail.message.title) {
+      itemTitle = detail.message.title;
+    } else if(detail.message.content) {
       // yes, we have content (probably chat) use it
-      itemTitle = this.props.detail.message.content;
+      itemTitle = detail.message.content;
     } else {
       // no content, just use message-type
-      itemTitle = this.props.detail.details.messageType;
+      itemTitle = detail.details.messageType;
     }
 
     return (
-      <React.Fragment key={this.props.key}>
+      <React.Fragment key={key}>
         <Collapsible
           trigger={
-            <div className="message-title-wrap" style={{borderColor: this.props.detail.details.from.forceColor}}>
-              <FontAwesomeIcon icon={this.props.detail.isOpen ? faMinus : faPlus} size="1x" />
+            <div className="message-title-wrap" style={{borderColor: detail.details.from.forceColor}}>
+              <FontAwesomeIcon icon={ expanded ? faMinus : faPlus } size="1x" />
               <div className="message-title">{itemTitle}</div>
               <div className="info-wrap">
-                <span className="info-body">{moment(this.props.detail.details.timestamp).format("HH:mm")}</span>
-                <Badge pill variant="dark">{this.props.detail.details.from.role}</Badge>
-                <Badge pill variant="secondary">{this.props.detail.details.messageType}</Badge>
-                {!this.state.hasBeenRead && <Badge pill variant="warning">Unread</Badge>}
+                <span className="info-body">{moment(detail.details.timestamp).format("HH:mm")}</span>
+                <Badge pill variant="dark">{detail.details.from.role}</Badge>
+                <Badge pill variant="secondary">{detail.details.messageType}</Badge>
+                {!hasBeenRead && <Badge pill variant="warning">Unread</Badge>}
               </div>
             </div>
           }
           transitionTime={200}
           easing={'ease-in-out'}
-          open={this.props.detail.isOpen}
+          open={expanded}
           onOpening={this.open}
           onClosing={this.close}
         >
-          <div key={`${this.props.key}-preview`} className="message-preview-player wrap"
-           style={{borderColor: this.props.detail.details.from.forceColor}}><MessagePreview detail={this.props.detail.message} from={this.props.detail.details.from} privateMessage={this.props.detail.details.privateMessage} /></div>
+          <div key={`${key}-preview`} className="message-preview-player wrap"
+           style={{borderColor: detail.details.from.forceColor}}><MessagePreview detail={detail.message} from={this.props.detail.details.from} privateMessage={this.props.detail.details.privateMessage} /></div>
         </Collapsible>
       </React.Fragment>
     )
