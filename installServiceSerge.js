dd3f52db9ad18167741b0686ebc367e01bc7e0a6
Ian Mayo <ian@planetmayo.com>
2019-08-20 16:04:06 +0100

    Change service name

--- installServiceSerge.js
+++ installServiceSerge.js
@@ -2,7 +2,7 @@ var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name: 'serge-server',
+  name: 'Serge Wargame',
   description: 'The Serge wargame server.',
   script: __dirname + '/server.js'
 });
