6c28cfe0c856d512ba2d25ff181541c70a1e3089
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-10-05 00:11:52 +0400

    update core configs

--- bin.js
+++ bin.js
@@ -14,7 +14,15 @@ const port = process.env.PORT || 8080;
 server(
   82,                                   //event emmiter max listeners
   { prefix: 'db/', adapter: 'websql' }, //PouchDb Options
-  {},                                   //cors options
+  {                                     //cors options
+    credentials: true,
+    origin: [
+      'https://serge-dev.herokuapp.com',
+      'http://localhost:3000',
+      'http://localhost:8080',
+      'http://localhost:8000',
+    ]
+  },
   './db',                               //database directory
   './img',                              //images directory
   port,                                 //port
--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const server = require("./server");
 server(
   82,                                   //event emmiter max listeners
   { prefix: 'db/', adapter: 'websql' }, //PouchDb Options
-  {},                                   //cors options
+  {                                     //cors options
+    credentials: true,
+    origin: [
+      'https://serge-dev.herokuapp.com',
+      'http://localhost:3000',
+      'http://localhost:8080',
+      'http://localhost:8000',
+    ]
+  },
   './db',                               //database directory
   './img',                              //images directory
   process.env.PORT || 8080,             //port
